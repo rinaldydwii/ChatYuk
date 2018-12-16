8577345ba68af870c503fa83e73883f05261ba7e
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-12-17 07:10:19 +0800

    join screen update

--- src/views/JoinScreen.js
+++ src/views/JoinScreen.js
@@ -1,29 +1,38 @@
 import React, { Component } from "react";
-import { View, Text, StatusBar, TouchableHighlight, TextInput, KeyboardAvoidingView } from "react-native";
-import { Header } from 'react-navigation'
+import { View, Text, StatusBar, TouchableHighlight, TextInput, AsyncStorage, ActivityIndicator, Dimensions, ToastAndroid } from "react-native";
 import { gorse, blackWhite, sunglow, earlyDawn, givry } from "../styles/colors";
 import Logo from "../assets/Logo";
+import firebaseApp from 'react-native-firebase'
 
 class JoinScreen extends Component {
     constructor() {
         super()
         this.state = {
-            name: ''
+            name: '',
+            isPressed: false
         }
     }
-    join = () => {
-        if (this.state.name)
-            this.props.navigation.navigate('App', {name: this.state.name})
+    join = async() => {
+        if (this.state.name) {
+            this.setState({isPressed: true})
+            firebaseApp.auth().signInAnonymously().then(async() => {
+                const userUid = firebaseApp.auth().currentUser.uid
+                await AsyncStorage.setItem('userToken', userUid)
+                await AsyncStorage.setItem('name', this.state.name)
+                this.props.navigation.navigate('App')
+            })
+        }
+        else ToastAndroid.show('Name is required!', ToastAndroid.SHORT)
     }
     render() {
         return (
             <View style={{flex: 1, backgroundColor: sunglow}}>
                 <StatusBar backgroundColor={sunglow}/>
-                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', position: 'absolute', top: 0, bottom: 298, left: 0, right: 0}}>
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', position: 'absolute', top: 100, left: 0, right: 0}}>
                     <Logo width={116} height={107} />
                     <Text style={{color: blackWhite, fontSize: 33, fontFamily: 'Lato Regular'}}><Text style={{color: gorse, fontFamily: 'Lato Heavy'}}>Chat</Text>Yuk!</Text>
                 </View>
-                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', paddingTop: 50}}>
+                <View style={{flex: 1, alignItems: 'center', paddingTop: (Dimensions.get('window').height/2) - (85/2)}}>
                     <TextInput 
                         style={{width: 250, fontSize: 14, color: earlyDawn, marginBottom: 20, textAlign: 'center', fontFamily: 'Lato Regular'}} 
                         underlineColorAndroid={earlyDawn}
@@ -32,9 +41,12 @@ class JoinScreen extends Component {
                         value={this.state.name}
                         onChangeText={(name) => this.setState({name})}
                     />
-                    <TouchableHighlight style={{backgroundColor: earlyDawn, borderRadius: 10, padding: 8, width: 120}} onPress={this.join} underlayColor={gorse}>
-                        <Text style={{fontSize: 14, color: sunglow, textAlign: 'center', fontFamily: 'Lato Regular'}}>Join</Text>
-                    </TouchableHighlight>
+                    { this.state.isPressed ? <ActivityIndicator color={earlyDawn} />
+                    : (
+                        <TouchableHighlight style={{backgroundColor: earlyDawn, borderRadius: 10, padding: 8, width: 120}} onPress={this.join} underlayColor={gorse}>
+                            <Text style={{fontSize: 14, color: sunglow, textAlign: 'center', fontFamily: 'Lato Regular'}}>Join</Text>
+                        </TouchableHighlight>
+                    )}
                 </View>
                 
             </View>
