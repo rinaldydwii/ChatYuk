c2fb902e38ff0d5dfbf0c85fdab50abddfb617ac
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-12-17 07:10:37 +0800

    chat screen update

--- src/views/ChatScreen.js
+++ src/views/ChatScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, StatusBar, Alert } from "react-native";
+import { View, StatusBar, Alert, AsyncStorage } from "react-native";
+import firebaseApp from 'react-native-firebase'
 import { GiftedChat, MessageText, Day, Time, Bubble, SystemMessage, Composer, InputToolbar, Send } from 'react-native-gifted-chat'
 import { sunglow, earlyDawn, putty, tana, alto, givry } from "../styles/colors";
 import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
@@ -7,47 +8,88 @@ import icoMoonConfig from '../selection.json';
 const Icon = createIconSetFromIcoMoon(icoMoonConfig);
 
 class ChatScreen extends Component {
+    static navigationOptions = ({navigation}) => {
+        return {
+            title: 'Chat Yuk!',
+            headerStyle: {
+                backgroundColor: sunglow,
+            }, 
+            headerTintColor: earlyDawn,
+            headerTitleStyle: {
+                textAlign: 'center',
+                fontFamily: 'Lato Regular'
+            },
+            headerRight: <Icon 
+                name="logout" 
+                size={26} 
+                color={earlyDawn} 
+                style={{marginRight: 14}} 
+                onPress={
+                    () => Alert.alert('Log Out', 'Are you sure want to log out?', [
+                        {text: "Cancel"}, 
+                        {text: "Ok", onPress: 
+                            async() => {
+                                firebaseApp.auth().signOut()
+                                await AsyncStorage.clear()
+                                navigation.navigate('Splash')
+                            }
+                        }
+                    ])} 
+                />
+        }
+    }
     constructor() {
         super()
         this.state = {
-            messages: []
+            messages: [],
+            user: {}
         }
+        this._isMounted = false
+    }
+    loadUser = async() => {
+        const userUid = await AsyncStorage.getItem('userToken')
+        const name = await AsyncStorage.getItem('name')
+        this.setState({user: {_id: userUid, name}})
+    }
+    loadMessages = () => {
+        firebaseApp.database()
+            .ref('messages')
+            .limitToLast(5)
+            .on('child_added', snapshot => {
+                const { key } = snapshot
+                const { createdAt, text, user } = snapshot.val()
+                let message = {
+                    _id: key,
+                    createdAt: new Date(createdAt),
+                    text,
+                    user
+                }
+                this.setState(previousState => ({
+                    messages: GiftedChat.append(previousState.messages, message),
+                }))
+            })
     }
     componentWillMount() {
-        this.setState({
-          messages: [
-            {
-                _id: "3",
-                text: 'Hello developer',
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'React Native',
-                },
-            },
-            {
-                _id: "2",
-                text: 'Hello developer',
-                createdAt: new Date(),
-                user: {
-                  _id: 1,
-                  name: 'Aldy',
-                },
-            },
-            {
-                _id: "1",
-                text: 'Welcome to ChatYuk!',
-                createdAt: new Date(),
-                system: true,
-                // Any additional custom parameters are passed through
-            },
-          ],
-        })
-      }
+        this.loadUser()
+        this.loadMessages()
+    }
+    componentDidMount() {
+        this._isMounted = true
+    }
+    componentWillUnmount() {
+        firebaseApp.database().ref('messages').off()
+        this._isMounted = false
+    }
     onSend = (messages = []) => {
-        this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }))
+        messages.map(item => {
+            const { text, user } = item
+            const message = {
+                text,
+                user,
+                createdAt: firebaseApp.database().getServerTime()
+            }
+            firebaseApp.database().ref('messages').push(message)
+        })
     }
     renderBubble (props) {
         return <Bubble 
@@ -143,38 +185,26 @@ class ChatScreen extends Component {
         return (
             <View style={{flex: 1, backgroundColor: sunglow}}>
                 <StatusBar backgroundColor={sunglow}/>
-                <GiftedChat
-                    messages={this.state.messages}
-                    onSend={this.onSend}
-                    user={{
-                        _id: 1,
-                    }}
-                    renderBubble={this.renderBubble}
-                    renderMessageText={this.renderMessageText}
-                    renderDay={this.renderDay}
-                    renderTime={this.renderTime}
-                    renderSystemMessage={this.renderSystemMessage}
-                    renderInputToolbar={this.renderInputToolbar}
-                    renderComposer={this.renderComposer}
-                    renderSend={this.renderSend}
-                    placeholder="Type a message..."
-                />
+                { this._isMounted ? 
+                    <GiftedChat
+                        messages={this.state.messages}
+                        onSend={this.onSend}
+                        user={this.state.user}
+                        renderBubble={this.renderBubble}
+                        renderMessageText={this.renderMessageText}
+                        renderDay={this.renderDay}
+                        renderTime={this.renderTime}
+                        renderSystemMessage={this.renderSystemMessage}
+                        renderInputToolbar={this.renderInputToolbar}
+                        renderComposer={this.renderComposer}
+                        renderSend={this.renderSend}
+                        placeholder="Type a message..."
+                    />
+                    : null
+                }
             </View>
         );
     }
-}
-
-ChatScreen.navigationOptions = {
-    title: 'Chat Yuk!',
-    headerStyle: {
-        backgroundColor: sunglow,
-    }, 
-    headerTintColor: earlyDawn,
-    headerTitleStyle: {
-        textAlign: 'center',
-        fontFamily: 'Lato Regular'
-    },
-    headerRight: <Icon name="logout" size={26} color={earlyDawn} style={{marginRight: 14}} onPress={() => Alert.alert('Test', 'Test')} />
 }
 
 export default ChatScreen;
